f1a0c2c8d9602e597cc304014c111e3257110765
Maple Lazuli <ada.lazuli@spaceforce.mil>
2026-03-15 00:40:31 -0400

    updated to have more reasonable starting categories

--- app/src/main/java/com/time/tracker/CategoryData.kt
+++ app/src/main/java/com/time/tracker/CategoryData.kt
@@ -2,7 +2,7 @@ package com.time.tracker
 
 val categoryMap = mapOf(
     "Language" to listOf("Vocabulary", "Grammar", "Listening"),
-    "Cyber" to listOf("Network Analysis", "Offensive Ops", "DNEA"),
+    "Cyber" to listOf("Network Analysis", "Offensive Ops", "Defensive Ops"),
     "Analysis" to listOf("Data Cleaning", "Visualization", "Modeling"),
     "Mathematics" to listOf("Statistics", "Calculus", "Probability")
 )
